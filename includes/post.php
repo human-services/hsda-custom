5182502b8cbcb0eb069e36f7e8eb543daa543de2
Kin Lane <info@apievangelist.com>
2017-09-04 20:06:46 -0400

    update with meta

--- includes/post.php
+++ includes/post.php
@@ -1,39 +1,58 @@
 <?php
 $ReturnObject = array();
 
+$override = 0;
+
 $request = $app->request();
 $_get = $request->params();
 $_get = filter_var_array($_get,FILTER_SANITIZE_STRING);
 $_body = $request->getBody();
 $_body = json_decode($_body,true);	
 $_body = filter_var_array($_body,FILTER_SANITIZE_STRING);
-
-if(isset($_body['name']))
-	{
 		
-	// Override any ID
-	$id = getGUID();
-	$_body['id'] = $id;
-	
-	// grab this path
-	$api = $openapi_yaml['paths'][$route];
-	
-	// grab this path
-	$definitions = $openapi_yaml['definitions'];
-	
-	// load up the parameters (type,name,description,default)
-	$parameters = $api[$verb]['parameters'];
-	
-	// load of up the responses
-	$responses = $api[$verb]['responses'];
-	$response_200 = $responses['200'];
-	
-	// grab our schema
-	$schema_ref = $response_200['schema']['items']['$ref'];
-	$schema = str_replace("#/definitions/","",$schema_ref);
-	$schema_properties = $definitions[$schema]['properties'];	
-	$schema = str_replace("_complete","",$schema);
+// Override any ID
+$local_id = getGUID();
+$_body['id'] = $local_id;
 
+// grab this path
+$api = $openapi['hsda-default']['paths'][$route];
+
+// grab this path
+$definitions = $openapi['hsda-default']['definitions'];
+
+// load up the parameters (type,name,description,default)
+$parameters = $api[$verb]['parameters'];
+
+// load of up the responses
+$responses = $api[$verb]['responses'];
+$response_200 = $responses['200'];
+
+// grab our schema
+$schema_ref = $response_200['schema']['items']['$ref'];
+$schema = str_replace("#/definitions/","",$schema_ref);
+$schema_properties = $definitions[$schema]['properties'];	
+$schema = str_replace("_complete","",$schema);
+
+$path_count_array = explode("/",$route);	
+$path_count = count($path_count_array);	
+$core_path = $path_count_array[1];
+$core_path = substr($core_path,0,strlen($core_path)-1);
+//echo "path: " . $core_path . "<br />";
+if(isset($_body[$core_path . "_id"])){ $_body[$core_path . "_id"] = $id; }
+
+// Load any pre extensions for this route
+if (file_exists($prepath)) 
+	{
+	include $prepath;
+	}
+
+// Meta
+include "meta/pre.php";	
+
+// override primary query
+if($override==0)
+	{
+	
 	// Fields
 	$field_string = "";
 	foreach($schema_properties as $field => $value)
@@ -69,25 +88,25 @@ if(isset($_body['name']))
 			}	
 		}	
 	if(substr($value_string,strlen($value_string)-1,1) == ","){ $value_string = substr($value_string,0,strlen($value_string)-1); }
-
+	
 	// Build The Query To Insert
 	$query = "INSERT INTO " . $schema ."(" . $field_string . ") VALUES(" . $value_string . ")";
 	//echo "\n" . $query . "\n";
-
+	
 	// Execute Query
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $response = $conn->exec($query);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$response = $conn->exec($query);
 	//echo $response . "\n";
 	
 	// We need to do something if fails -- return proper status code
-    if($response==1)
-    	{
-    	// Success
-    	}
-    else
-    	{
-    	// INSERT FAILED -- what do we do
-    	}
+	if($response==1)
+		{
+		// Success
+		}
+	else
+		{
+		// INSERT FAILED -- what do we do
+		}
 	// Return Values
 	$F = array();
 	foreach($schema_properties as $field => $value)
@@ -112,14 +131,14 @@ if(isset($_body['name']))
 			$sub_schema_properties = $definitions[$sub_schema]['properties'];
 			//echo $sub_schema . "\n";
 			//var_dump($sub_schema_properties);	
-
+	
 			foreach($_body[$field] as $sub_body)
 				{
-
+	
 				// Override any ID
 				$sub_id = getGUID();
-				$sub_body['id'] = $id;	
-				$sub_body[$core_path . '_id'] = $sub_id;	
+				$sub_body['id'] = $sub_id;	
+				$sub_body[$core_path . '_id'] = $local_id;	
 				
 				// Fields
 				$field_string = "";
@@ -172,9 +191,18 @@ if(isset($_body['name']))
 			
 			}				
 		}	
-
-	$ReturnObject = $F;
 	}
+	
+$ReturnObject = $F;
+	
+// Load any post extensions for this path	
+if (file_exists($postpath)) 
+	{
+	include $postpath;
+	}	
+	
+// Meta
+include "meta/post.php";	
 
 //echo $head['ACCEPT'] . "<br />";
 if(isset($head['ACCEPT']) && $head['ACCEPT'] == 'text/csv')
